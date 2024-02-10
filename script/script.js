b58727e4d937d7f2e335fd6800f7a1dafe54d366
Danny-607 <danielhudson1294@gmail.com>
2024-02-10 12:59:54 +0000

    Added functionality
    Changed variable names to be more meaningful
    Added the functionality to see payments broken up between interest and
    initial loan
    Changed the way of creating the table to look and function better for
    styling

--- script/script.js
+++ script/script.js
@@ -1,34 +1,40 @@
 var table = document.getElementById("table");
 
 document.querySelector("form").addEventListener("submit", function(e) {
-    e.preventDefault();
-
-let principal = document.getElementById("principal").value;
+  e.preventDefault();
+  let principal = document.getElementById("principal").value;
   let downPayment = document.getElementById("downPayment").value;
   let interestRate = document.getElementById("interestRate").value * 0.01;
   let loanTerm = document.getElementById("loanTerm").value;
   let loanTermMonth = loanTerm * 12;
   let loan = principal - downPayment;
 
-  var monthlyInterestRate = interestRate/12;
-  var monthlyPayment = loan * (monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, -loanTermMonth));
-  var total = monthlyPayment * loanTermMonth;
 
+  let monthlyInterestRate = interestRate/12;
+  let monthlyPayment = loan * (monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, -loanTermMonth));
+  let total = monthlyPayment * loanTermMonth;
+  let interestPayment = total - loan
 
-  generateTable(loanTermMonth, monthlyPayment, total);
+  generateTable(loanTermMonth, monthlyPayment, total, interestPayment,loan);
 });
 
-function generateTable(loanTermMonth, monthlyPayment, total){
-  var leftToRepay = total;
-    for (let i = 0; i < loanTermMonth; i++){
-      // console.log(i)
+function generateTable(loanTermMonth, monthlyPayment, total, interestPayment,loan){
+  let leftToRepay = total;
+  let tableRows = "";
+  let monthlyInterestRepayment = interestPayment / loanTermMonth
+  let monthlyLoanRepayment = loan / loanTermMonth
+  console.log(monthlyInterestRepayment.toFixed(2),monthlyLoanRepayment.toFixed(2))
+  for (let i = 0; i < loanTermMonth; i++){
 
-      leftToRepay = (leftToRepay - monthlyPayment).toFixed(2);
-      console.log(leftToRepay, monthlyPayment.toFixed(2), i);
-      if (leftToRepay < 0) {
-        leftToRepay = 0;
-      }
-      table.innerHTML += `<tr> <td>${i + 1}</td> <td>${leftToRepay}</td> <td>${monthlyPayment.toFixed(2)}</td>  </tr>`;
+    leftToRepay -= monthlyPayment;
+
+    if (leftToRepay < 0) {
+      leftToRepay = 0;
+
     }
 
+    tableRows += `<tr> <td>${i + 1}</td> <td>${leftToRepay.toFixed(2)}</td> <td>${monthlyLoanRepayment.toFixed(2)}</td> <td>${monthlyInterestRepayment.toFixed(2)}</td> <td>${monthlyPayment.toFixed(2)}</td> </tr>`;
   }
+
+  table.innerHTML = `<tr> <th>Month</th> <th>Remaining Loan</th> <th>Principal Repayment</th> <th>Interest Payment</th> <th>Total Monthly Payment</th> </tr>` + tableRows;
+}
