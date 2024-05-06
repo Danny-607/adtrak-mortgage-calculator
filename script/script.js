3dfe89a9981adc838f56253952eb92ccb8ef4406
Danny-607 <danielhudson1294@gmail.com>
2024-05-07 00:16:59 +0100

    Added popup, changed styling of form and popup, rebuilt chart

--- script/script.js
+++ script/script.js
@@ -1,30 +1,74 @@
-var table = document.getElementById("table");
+var ctx = document.getElementById('mortgageChart').getContext('2d');
+var chartInstance = null; // This will hold the chart instance
+var modal = document.getElementById('modal');
+var yearlyButton = document.getElementById('yearlyButton');
+var monthlyButton = document.getElementById('monthlyButton');
+var monthlyText = document.getElementById('month')
+var yearlyText = document.getElementById('year')
 
-document.querySelector("form").addEventListener("submit", function(e) {
+document.querySelector("form").addEventListener("submit", function (e) {
   e.preventDefault();
-  let principal = document.getElementById("principal").value;
-  let downPayment = document.getElementById("downPayment").value;
-  let interestRate = document.getElementById("interestRate").value * 0.01;
-  let loanTerm = document.getElementById("loanTerm").value;
+  modal.classList.remove('hidden');
+
+  let principal = parseFloat(document.getElementById("principal").value);
+  let downPayment = parseFloat(document.getElementById("downPayment").value);
+  let interestRate = parseFloat(document.getElementById("interestRate").value) * 0.01;
+  let loanTerm = parseInt(document.getElementById("loanTerm").value);
   let loanTermMonth = loanTerm * 12;
   let loan = principal - downPayment;
 
-
-  let monthlyInterestRate = interestRate/12;
+  let monthlyInterestRate = interestRate / 12;
   let monthlyPayment = loan * (monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, -loanTermMonth));
   let total = monthlyPayment * loanTermMonth;
-  let interestPayment = total - loan
+  let interestPayment = total - loan;
 
-  generateTable(loanTermMonth, monthlyPayment, total, interestPayment,loan);
+
+  yearlyButton.onclick = function () { yearly(total, loanTerm, interestPayment, loan) };
+
+  monthlyButton.onclick = function () { monthly(total, interestPayment, monthlyPayment, loanTermMonth, loan) };
+  yearly(total, loanTerm, interestPayment, loan);
+
+  monthlyText.textContent = `Monthly repayment amount ${monthlyPayment.toFixed(2)}`;
+  yearlyText.textContent = `Yearly repayment amount ${(monthlyPayment * 12).toFixed(2)}`;
+
 });
 
-function generateTable(loanTermMonth, monthlyPayment, total, interestPayment,loan){
+// Show monthly payments on chart
+function monthly(total, interestPayment, monthlyPayment, loanTermMonth, loan) {
+  let data = [];
+  let interestData = [];
+  let loanData = [];
   let leftToRepay = total;
-  let tableRows = "";
+
+
   let monthlyInterestRepayment = interestPayment / loanTermMonth
   let monthlyLoanRepayment = loan / loanTermMonth
-  console.log(monthlyInterestRepayment.toFixed(2),monthlyLoanRepayment.toFixed(2))
-  for (let i = 0; i < loanTermMonth; i++){
+
+  for (let i = 0; i < loanTermMonth; i++) {
+
+    interestPayment -= monthlyInterestRepayment;
+
+    if (interestPayment < 0) {
+      interestPayment = 0;
+
+    }
+    interestData.push(interestPayment.toFixed(2));
+  }
+
+
+  for (let i = 0; i < loanTermMonth; i++) {
+
+    loan -= monthlyLoanRepayment;
+
+    if (loan < 0) {
+      loan = 0;
+
+    }
+    loanData.push(loan.toFixed(2));
+  }
+
+
+  for (let i = 0; i < loanTermMonth; i++) {
 
     leftToRepay -= monthlyPayment;
 
@@ -32,9 +76,112 @@ function generateTable(loanTermMonth, monthlyPayment, total, interestPayment,loa
       leftToRepay = 0;
 
     }
+    data.push(leftToRepay.toFixed(2));
+  }
+  generateChart(data, loanTermMonth, interestData, loanData)
+}
 
-    tableRows += `<tr> <td>${i + 1}</td> <td>${leftToRepay.toFixed(2)}</td> <td>${monthlyLoanRepayment.toFixed(2)}</td> <td>${monthlyInterestRepayment.toFixed(2)}</td> <td>${monthlyPayment.toFixed(2)}</td> </tr>`;
+// Show yearly payments on chart
+function yearly(total, loanTerm, interestPayment, loan) {
+  var data = [];
+  let interestData = [];
+  let loanData = [];
+  var totalYearlyLoanRepayment = total / loanTerm;
+  var leftToRepay = total;
+  let yearlyInterestRepayment = interestPayment / loanTerm
+  let yearlyLoanRepayment = loan / loanTerm
+
+  for (let i = 0; i < loanTerm; i++) {
+
+    interestPayment -= yearlyInterestRepayment;
+
+    if (interestPayment < 0) {
+      interestPayment = 0;
+
+    }
+    interestData.push(interestPayment.toFixed(2));
   }
 
-  table.innerHTML = `<tr> <th>Month</th> <th>Remaining Loan</th> <th>Principal Repayment</th> <th>Interest Payment</th> <th>Total Monthly Payment</th> </tr>` + tableRows;
+
+  for (let i = 0; i < loanTerm; i++) {
+
+    loan -= yearlyLoanRepayment;
+
+    if (loan < 0) {
+      loan = 0;
+
+    }
+    loanData.push(loan.toFixed(2));
+  }
+  for (let i = 0; i < loanTerm; i++) {
+    leftToRepay -= totalYearlyLoanRepayment;
+    if (leftToRepay < 0) {
+      leftToRepay = 0;
+    }
+    data.push(leftToRepay.toFixed(2));
+  }
+
+  generateChart(data, loanTerm, interestData, loanData);
 }
+
+// generates chart
+function generateChart(data, term, interestData, loanData) {
+  // Destroy previous chart instance if exists
+  if (chartInstance) {
+    chartInstance.destroy();
+  }
+
+  // Generate new chart
+  chartInstance = new Chart(ctx, {
+    type: 'line',
+    data: {
+      // sets the x-axis labels to the term of the loan
+      labels: Array.from({ length: term }, (_, i) => ` ${i + 1}`),
+      datasets: [{
+        label: 'Remaining Balance',
+        // make chart js background red and border red 
+
+
+        backgroundColor: 'rgba(0,255,0)',
+        borderColor: 'rgba(0,255,0)',
+        data: data
+      },
+      {
+        label: 'Interest Payment',
+        backgroundColor: 'rgba(0,0,255)',
+
+        borderColor: 'rgba(0,0,255)',
+        data: interestData // This should be another array of data for the second line
+      },
+      {
+        label: 'Loan Payment',
+        backgroundColor: 'rgba(255,0,0)',
+        borderColor: 'rgb(255,0,0)', // Red
+        data: loanData // This should be another array of data for the second line
+      }
+
+      ],
+
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+}
+
+// close modal by clicking outside the area of the modal
+window.onclick = function (event) {
+  if (!modal.contains(event.target)) {
+    modal.classList.add('hidden');
+  }
+}
+
+// close modal by pressing button
+document.getElementById('close-btn').addEventListener('click', function () {
+  modal.classList.add('hidden');
+});
